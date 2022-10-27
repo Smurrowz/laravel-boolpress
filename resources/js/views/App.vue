cfff66522860ef94d929046f434950946ebec72d
Smurrowz <nigeldn1995@gmail.com>
2022-10-27 13:02:37 +0200

    Oye como va

---
+++ resources/js/views/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, ducimus omnis! Maiores fugit debitis quod, rerum hic harum! Sequi blanditiis enim magni impedit, nisi sint iure placeat eum hic reprehenderit!</p>
+    
+  </div>
+</template>
+
+
+<script>
+export default {
+ data(){
+  return{
+    title: 'sup'
+  }
+ }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
